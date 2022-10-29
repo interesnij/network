38d176bc4e5239c14a05bf8ac4cb15fb86bf0cf3
interesnij <ochkarik1983@mail.ru>
2022-10-29 12:13:41 +0300

    J

--- crates/posts/src/utils/mod.rs
+++ crates/posts/src/utils/mod.rs
@@ -69,39 +69,39 @@ pub fn get_count_for_ru_alt(count: i32, word1: String, word2: String, word3: Str
 pub fn get_post_list(pk: i32) -> Result<PostList, Error> {
     use crate::schema::post_lists::dsl::post_lists;
     let _connection = establish_connection();
-    return post_lists
+    return Ok(post_lists
         .filter(schema::post_lists::id.eq(pk))
-        .first::<PostList>(&_connection)?;
+        .first::<PostList>(&_connection)?);
 }
 
 pub fn get_post(pk: i32) -> Result<Post, Error> {
     use crate::schema::posts::dsl::posts;
     let _connection = establish_connection();
-    return posts
+    return Ok(posts
         .filter(schema::posts::id.eq(pk))
-        .first::<Post>(&_connection)?;
+        .first::<Post>(&_connection)?);
 }
 
 pub fn get_post_comment(pk: i32) -> Result<PostComment, Error> {
     use crate::schema::post_comments::dsl::post_comments;
     let _connection = establish_connection();
-    return post_comments
+    return Ok(post_comments
         .filter(schema::post_comments::id.eq(pk))
-        .first::<PostComment>(&_connection)?;
+        .first::<PostComment>(&_connection)?);
 }
 
 pub fn get_user(pk: i32) -> Result<User, Error> {
     use crate::schema::users::dsl::users;
     let _connection = establish_connection();
-    return users
+    return Ok(users
         .filter(schema::users::id.eq(pk))
-        .first::<User>(&_connection)?;
+        .first::<User>(&_connection)?);
 }
 
 pub fn get_community(pk: i32) -> Result<Community, Error> {
     use crate::schema::communitys::dsl::communitys;
     let _connection = establish_connection();
-    return communitys
+    return Ok(communitys
         .filter(schema::communitys::id.eq(pk))
-        .first::<Community>(&_connection)?;
+        .first::<Community>(&_connection)?);
 }
