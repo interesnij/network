ab253d1d258234e043d09a1110560aa15aeb5438
interesnij <ochkarik1983@mail.ru>
2022-11-01 17:20:59 +0300

    J

--- crates/posts/src/routes.rs
+++ crates/posts/src/routes.rs
@@ -2,12 +2,12 @@ use actix_web::web;
 
 use crate::views::{
     pages,
-    //progs,
+    progs,
 };
 
 pub fn routes(cfg: &mut web::ServiceConfig) {
     cfg
     .configure(pages::pages_routes)
-    //.configure(progs::progs_routes)
+    .configure(progs::progs_routes)
     ;
 }
