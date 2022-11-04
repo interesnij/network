a575720b2e5899351aa190704f3bc583c9710943
interesnij <ochkarik1983@mail.ru>
2022-11-04 14:42:09 +0300

    J

--- crates/posts/src/views/item_progs.rs
+++ crates/posts/src/views/item_progs.rs
@@ -261,7 +261,7 @@ pub async fn copy_post(data: Json<DataCopyPost>) -> Result<Json<i16>, Error> {
             Err(Error::BadRequest(_tuple.1))
         }
         else {
-            let _res = block(move || item.copy_item(data.user_id, data.lists)).await?;
+            let _res = block(move || item.copy_item(data.lists)).await?;
             Ok(Json(_res))
         }
     }
@@ -272,7 +272,7 @@ pub async fn copy_post(data: Json<DataCopyPost>) -> Result<Json<i16>, Error> {
             Err(Error::BadRequest(_tuple.1))
         }
         else {
-            let _res = block(move || item.copy_item(data.user_id, data.lists)).await?;
+            let _res = block(move || item.copy_item(data.lists)).await?;
             Ok(Json(_res))
         }
     }
