429ebf5307b4fe51534b13c6c1024693cfcf974e
interesnij <ochkarik1983@mail.ru>
2022-11-13 14:07:31 +0300

    h

--- crates/users/src/views/load_pages.rs
+++ crates/users/src/views/load_pages.rs
@@ -66,7 +66,7 @@ pub async fn friends_load(req: HttpRequest) -> impl Responder {
             }).await;
             let resp = match _res {
                 Ok(_ok) => _ok,
-                Err(_) => Err(403),
+                Err(_e) => _e,
             };
             HttpResponse::Ok().body(resp)
         }
