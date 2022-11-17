83f4fdef8f4fffb746b814043b37a19d0e8f139c
interesnij <ochkarik1983@mail.ru>
2022-11-17 20:26:21 +0300

    J

--- crates/communities/src/errors.rs
+++ crates/communities/src/errors.rs
@@ -13,14 +13,14 @@ pub enum AuthError {
     #[display(fmt = "DuplicateValue: {}", _0)]
     DuplicateValue(String),
 
-    #[display(fmt = "BadId")]
-    BadId,
+    //#[display(fmt = "BadId")]
+    //BadId,
 
     #[display(fmt = "NotFound: {}", _0)]
     NotFound(String),
 
-    #[display(fmt = "ProcessError: {}", _0)]
-    ProcessError(String),
+    //#[display(fmt = "ProcessError: {}", _0)]
+    //ProcessError(String),
 
     #[display(fmt = "AuthenticationError: {}", _0)]
     AuthenticationError(String),
@@ -147,7 +147,7 @@ impl From<DBError> for Error {
 }
 
 impl From<BlockingError> for Error {
-    fn from(_error: BlockingError) -> Error {
+    fn from(error: BlockingError) -> Error {
         //error!("Thread blocking error {:?}", error);
         Error::BlockingError("Thread blocking error".into())
     }
