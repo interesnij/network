1f9f8ea584a64fc68c72435a9c02639dc7ac5373
interesnij <ochkarik1983@mail.ru>
2022-11-20 15:06:28 +0300

    J

--- crates/api/src/views/users/pages.rs
+++ crates/api/src/views/users/pages.rs
@@ -43,7 +43,7 @@ pub async fn all_users_page(req: HttpRequest) -> Json<String> {
         },
         Err(_error) => {
             println!("status {}", _error.status().unwrap().as_str());
-            Json(_error.status().unwrap().to_string())
+            Json(_error.status().to_string())
         },
     }
 }
