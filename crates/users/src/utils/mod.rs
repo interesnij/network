c69f5d0afe21dc5dc22185fbc55472af964aeadf
interesnij <ochkarik1983@mail.ru>
2022-11-20 16:24:40 +0300

    J

--- crates/users/src/utils/mod.rs
+++ crates/users/src/utils/mod.rs
@@ -142,7 +142,7 @@ pub fn get_user_owner_data (
             }
         }
         // test case!!!
-        else if token.clone() == Some("11".to_string()) {
+        else if &token == &Some("11".to_string()) {
             return (None, 0);
         }
         else {
