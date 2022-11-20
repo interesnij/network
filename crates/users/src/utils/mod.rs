773adccd8e74111425f12ccfbb91950ccef618b8
interesnij <ochkarik1983@mail.ru>
2022-11-20 16:25:39 +0300

    J

--- crates/users/src/utils/mod.rs
+++ crates/users/src/utils/mod.rs
@@ -113,6 +113,7 @@ pub fn get_user_owner_data (
     if token.is_some() {
         use crate::schema::owners::dsl::owners;
         let _connection = establish_connection();
+        let _tok = &token;
         let owner_res = owners
             .filter(schema::owners::service_key.eq(token.unwrap()))
             .first::<Owner>(&_connection);
@@ -142,7 +143,7 @@ pub fn get_user_owner_data (
             }
         }
         // test case!!!
-        else if &token == &Some("11".to_string()) {
+        else if &_tok == &Some("11".to_string()) {
             return (None, 0);
         }
         else {
