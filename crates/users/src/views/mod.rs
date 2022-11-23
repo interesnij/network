f08f26b398115189ffd19c60ac733a8122f08d5b
interesnij <ochkarik1983@mail.ru>
2022-11-23 13:04:36 +0300

    J

--- crates/users/src/views/mod.rs
+++ crates/users/src/views/mod.rs
@@ -1,11 +1,11 @@
 pub mod pages;
 pub mod load_pages;
 pub mod auth;
-pub mod owner;
+pub mod owner_progs;
 
 pub use self::{
     pages::*,
     load_pages::*,
     auth::*,
-    owner::*,
+    owner_progs::*,
 };
