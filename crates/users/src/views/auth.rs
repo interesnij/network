817b4424d9b4be87bd53241d7aa30a27fa34564f
interesnij <ochkarik1983@mail.ru>
2022-11-26 16:47:52 +0300

    J

--- crates/users/src/views/auth.rs
+++ crates/users/src/views/auth.rs
@@ -360,7 +360,7 @@ pub struct OptionPhoneCodeJson {
     pub code:  Option<String>,
 }
 pub async fn phone_verify(data: web::Json<OptionPhoneCodeJson>) -> Result<Json<usize>, Error> {
-    let (err, user_id) = get_user_owner_data(data.token.clone(), Some(0), 0);
+    let (err, user_id) = get_user_owner_data(data.token.clone(), None, 0);
     if err.is_some() || (user_id != 0) {
         return Err(Error::BadRequest(err.unwrap()));
     } 
