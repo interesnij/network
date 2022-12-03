2dc2f46eebcfbe4263dec682f8ed57f0f9a1a2b3
interesnij <ochkarik1983@mail.ru>
2022-12-03 18:50:37 +0300

    J

--- crates/communities/src/models/user.rs
+++ crates/communities/src/models/user.rs
@@ -728,8 +728,8 @@ impl User {
 
         let _connection = establish_connection();
         return friends
-            .filter(schema::friends::u.user_id))
-            .filter(schema::friends::target_id.eq(userser_id.eq(self_id))
+            .filter(schema::friends::user_id.eq(user_id)))
+            .filter(schema::friends::target_id.eq(self_id))
             .select(schema::friends::id)
             .first::<i32>(&_connection).is_ok();
     }
