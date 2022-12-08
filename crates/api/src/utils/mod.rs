b4363dcba0cae5160c6bca8f34ceaece5c5158b1
interesnij <ochkarik1983@mail.ru>
2022-12-08 21:01:17 +0300

    J

--- crates/api/src/utils/mod.rs
+++ crates/api/src/utils/mod.rs
@@ -8,13 +8,13 @@ use diesel::{
 };
 
 // url сервера, куда шлем запросы сервиса записей
-static POSTS_URL: &str = "http:194.58.90.123:9003";
-static USERS_URL: &str = "http:194.58.90.123:9001";
-static COMMUNITIES_URL: &str = "http:194.58.90.123:9002";
+pub static POSTS_URL: &str = "http:194.58.90.123:9003";
+pub static USERS_URL: &str = "http:194.58.90.123:9001";
+pub static COMMUNITIES_URL: &str = "http:194.58.90.123:9002";
 
 // список url сервисов, на которых присутствуют копии пользователей.
 // нужно, к примеру, для синхронизации данных пользователей.
-static USERS_SERVICES: Vec<&str> = [
+pub static USERS_SERVICES: Vec<&str> = [
     "194.58.90.123:9002",
     "194.58.90.123:9003",
 ].to_vec();
