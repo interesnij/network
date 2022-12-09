d54914b2acf9bfdbc6fced3af507bbd9f7be940a
interesnij <ochkarik1983@mail.ru>
2022-12-09 11:28:41 +0300

    H

--- crates/api/src/utils/mod.rs
+++ crates/api/src/utils/mod.rs
@@ -14,18 +14,18 @@ pub const COMMUNITIES_URL: &str = "http:194.58.90.123:9002";
 
 // список url сервисов, на которых присутствуют копии пользователей.
 // нужно, к примеру, для синхронизации данных пользователей.
-pub const USERS_SERVICES: &'static [&'static str] = [
+pub const USERS_SERVICES: &'static [&'static str] = &[
     "194.58.90.123:9002",
     "194.58.90.123:9003",
 ].to_vec();
 
 // список url сервисов, на которых присутствуют копии сообществ.
-pub const COMMUNITIES_SERVICES: &'static [&'static str] = [
+pub const COMMUNITIES_SERVICES: &'static [&'static str] = &[
     "194.58.90.123:9003",
 ].to_vec();
 
 // список url сервисов, на которых присутствуют копии элементов для прикрепов и папок.
-pub const ATTACH_SERVICES: &'static [&'static str] = [
+pub const ATTACH_SERVICES: &'static [&'static str] = &[
     "194.58.90.123:9003",
 ].to_vec();
 
