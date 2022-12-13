6dc849597221c81e3fecc8e06559a006919b989c
interesnij <ochkarik1983@mail.ru>
2022-12-13 13:11:06 +0300

    J

--- crates/photos/src/main.rs
+++ crates/photos/src/main.rs
@@ -24,8 +24,8 @@ async fn main() -> std::io::Result<()> {
     env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
 
     let config_to_static_server = ConfigToStaticServer::parse();
-    let ConfigToStaticServer { my_address, my_port, to_static_server } = config_to_static_server.clone();
-    info!("Listening on {my_address}:{my_port}");
+    let ConfigToStaticServer { address, port, to } = config_to_static_server.clone();
+    info!("Listening on {address}:{port}");
     info!("Proxying requests to static_server {to_static_server}");
 
     HttpServer::new(move || {
