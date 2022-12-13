f8891feeca2705dc50b66109086a17c975e928af
interesnij <ochkarik1983@mail.ru>
2022-12-13 10:02:05 +0300

    J

--- crates/photos/src/main.rs
+++ crates/photos/src/main.rs
@@ -16,18 +16,28 @@ async fn main() -> std::io::Result<()> {
     use actix_web::{App, HttpServer, web::JsonConfig};
     use actix_cors::Cors;
     use crate::routes::routes;
-    use crate::utils::util::google_config;
+    use crate::utils::{proxy, Config};
+
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+
+    let config = Config::parse();
+    let Config { address, port, to } = config.clone();
+    info!("Listening on {address}:{port}");
+    info!("Proxying requests to {to}");
 
     HttpServer::new(|| {
+        let http_client = awc::Client::default();
         let cors = Cors::default()
             .allowed_origin("194.58.90.123:8000")
             .allowed_methods(vec!["GET", "POST"])
             .max_age(3600);
         App::new()
+            .app_data(Data::new(config.clone()))
+            .app_data(Data::new(http_client))
             .app_data(JsonConfig::default().limit(4096))
             .wrap(cors)
             .configure(routes)
-            .configure(google_config)
+            .service(web::resource("{path:.*}").to(proxy))
     })
     .bind("194.58.90.123:9004")?
     .run()
