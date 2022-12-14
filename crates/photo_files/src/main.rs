a399ec60f0a9002f1e7983b6fb3d6223c784f79d
interesnij <ochkarik1983@mail.ru>
2022-12-14 13:49:56 +0300

    J

--- crates/photo_files/src/main.rs
+++ crates/photo_files/src/main.rs
@@ -1,25 +1,13 @@
-//#[macro_use]
-//extern crate diesel;
-
-//use std::sync::Arc;
-//use dotenv::dotenv;
-//use std::env;
 use actix_web::{
-    //middleware::Logger,
     Result,
     HttpRequest,
+    web::Json,
 };
 use actix_cors::Cors;
 use actix_files::NamedFile;
 use std::path::PathBuf;
 
-//mod models;
-//mod views;
-//mod utils;
-//mod schema;
-//mod errors;
-//mod routes;
-
+mod utils;
 
 
 async fn get_file(req: HttpRequest) -> Result<NamedFile> {
@@ -27,9 +15,16 @@ async fn get_file(req: HttpRequest) -> Result<NamedFile> {
     Ok(NamedFile::open(path)?)
 }
 
+pub async fn create_files(mut payload: Multipart, list_id: web::Path<i32>) -> 
+    Result<Json<Vec<String>> {
+        use crate::utils::files_form;
+
+        let form = files_form(payload.borrow_mut(), list_id).await;
+        Ok(Json(form.files))
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    //use crate::routes::routes;
     use actix_web::{web, App, HttpServer};
 
     HttpServer::new(move || {
@@ -40,8 +35,8 @@ async fn main() -> std::io::Result<()> {
 
         App::new()
             .wrap(cors)
-            .route("/{filename:.*}", web::get().to(get_file))
-            //.configure(routes)
+            .route("/{filename:.*}", web::post().to(get_file))
+            .route("/create_files/{list_id}", web::get().to(get_file))
 
     })
     .bind("194.58.90.123:9050")?
