3fc655f63743b4510cb061440716ed1520fbb10f
interesnij <ochkarik1983@mail.ru>
2022-12-15 17:57:49 +0300

    J

--- crates/photos/src/models/sql/photos.sql
+++ crates/photos/src/models/sql/photos.sql
@@ -563,6 +563,9 @@ CREATE TABLE owners (
 );
 CREATE INDEX item_service_key_index ON owners (service_key);
 
+INSERT INTO owners (id, user_id, name, types, secret_key, service_key, is_active)
+VALUES (1, 1, 'Браузерное приложение', 1, '%n%#Nv!|y9nU', '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', true ) ON CONFLICT DO NOTHING;
+
 -- связь сервисов токенов с токенами -------
 CREATE TABLE owner_services_items (
     id          SERIAL PRIMARY KEY, -- id
@@ -571,6 +574,11 @@ CREATE TABLE owner_services_items (
 );
 CREATE UNIQUE INDEX owner_services_items_unq ON owner_services_items (owner_id, service_id);
 
+-- создадим варианты для токенов, чтобы сто раз не добавлять
+INSERT INTO owner_services (id, types, name)
+VALUES (1, 8, 'Фотографии') ON CONFLICT DO NOTHING;
+INSERT INTO owner_services (id, types, name)
+VALUES (2, 38, 'Управление фотографиями') ON CONFLICT DO NOTHING;
 
 /*
 объекты других сервисов
