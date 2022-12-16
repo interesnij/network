9a0e61c6da35e145412294ed5ab70f6893d23af7
interesnij <ochkarik1983@mail.ru>
2022-12-16 17:05:53 +0300

    J

--- crates/photo_files/src/utils/forms.rs
+++ crates/photo_files/src/utils/forms.rs
@@ -81,7 +81,7 @@ pub async fn files_form(payload: &mut Multipart, list_id: i32) -> FileForm {
                     .expect("E");
             };
 
-            let folder = "media/ser1/".to_owned() + &list_id.to_string() + &"/".to_string();
+            let folder = Path::new("media/ser1/".to_owned() + &list_id.to_string() + &"/".to_string());
 
             let input = ImageResource::from_path(file.path.clone());
             let mut output = None;
