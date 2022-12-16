bb5f567ef7f6a53e15bfbe318c2c85458850b099
interesnij <ochkarik1983@mail.ru>
2022-12-16 15:10:50 +0300

    J

--- crates/photo_files/src/utils/forms.rs
+++ crates/photo_files/src/utils/forms.rs
@@ -61,7 +61,7 @@ pub async fn files_form(payload: &mut Multipart, list_id: i32) -> FileForm {
             if _new_path != "" { 
                 let file = UploadedFiles::new(_new_path.to_string(), list_id);
                 let file_path = file.path.clone();
-                let mut f = web::block(move || std::fs::File::create(&file_path).expect("E"))
+                let mut f = web::block(move || std::fs::File::create(file.path.clone()).expect("E"))
                     .await
                     .unwrap();
                 while let Some(chunk) = field.next().await {
@@ -72,7 +72,7 @@ pub async fn files_form(payload: &mut Multipart, list_id: i32) -> FileForm {
                         .expect("E");
                 };
                 _files.push(file.clone());
-                form.files.push(file_path.clone().replace("./","/"));
+                form.files.push(file.path.clone()).replace("./","/"));
             }
         }
     }
