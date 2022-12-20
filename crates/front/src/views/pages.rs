23d4792b32da8d3dbf5846992dbe57c66d85d085
interesnij <ochkarik1983@mail.ru>
2022-12-20 18:59:30 +0300

    J

--- crates/front/src/views/pages.rs
+++ crates/front/src/views/pages.rs
@@ -4,12 +4,19 @@ use actix_web::{
     Responder,
     web,
 };
+use actix_identity::Identity;
+
 
 pub fn pages_urls(config: &mut web::ServiceConfig) {
     config.route("/", web::get().to(index_page));
 }
 
-pub async fn index_page() -> impl Responder {
+pub async fn index_page(user: Option<Identity>) -> impl Responder {
+    if let Some(user) = user {
+        println!("Welcome! {}", user.id().unwrap());
+    } else {
+        println!("Welcome Anonymous!".to_owned());
+    }
     HttpResponse::Ok().body(
         "<div style='background: #ccc;position:absolute;top:0;left:0;right:0;bottom:0'>
             <p style='text-align: center'>
