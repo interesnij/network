ba62c2efa52f5a8844a7cf56ba7d4e5b793207ce
interesnij <ochkarik1983@mail.ru>
2022-12-22 12:33:45 +0300

    J

--- crates/front/src/main.rs
+++ crates/front/src/main.rs
@@ -5,14 +5,34 @@ use actix_web::{
     HttpRequest,
     HttpServer,
     cookie::Key,
+    dev::ServiceRequest,
+    Error,
 };
 use actix_cors::Cors;
 use actix_identity::IdentityMiddleware;
 use actix_redis::RedisSession;
+use actix_web_httpauth::{
+    extractors::{bearer::{BearerAuth, Config}, AuthenticationError},
+    middleware::HttpAuthentication,
+};
+
 
 mod views;
 //mod utils;
 mod routes;
+
+
+async fn validator (
+    req: ServiceRequest,
+    credentials: BearerAuth,
+) -> Result<ServiceRequest, (Error, ServiceRequest)> {
+    let config = req.app_data::<Config>().cloned().unwrap_or_default();
+    match auth::get_token_data(credentials.token()).await {
+        Ok(_) => Ok(req),
+        Err(_) => Err((AuthenticationError::from(config).into(), req)),
+    }
+}
+
 
 #[derive(Clone)]
 pub struct AppState {
@@ -30,9 +50,11 @@ async fn main() -> std::io::Result<()> {
 
     HttpServer::new(move || {
         let _files = Files::new("/static", "static/").show_files_listing();
+        let auth = HttpAuthentication::bearer(validator);
         App::new() 
             .app_data(web::Data::new(app_state.to_owned()))
             .wrap(IdentityMiddleware::default())
+            .wrap(auth)
             .wrap(RedisSession::new("127.0.0.1:6379", &[0; 32]))
             .configure(routes)
             .service(_files)
