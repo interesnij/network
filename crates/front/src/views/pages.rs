337ca1d1d88ce4631122511b4e7a62c78eef68ea
interesnij <ochkarik1983@mail.ru>
2022-12-23 17:56:23 +0300

    J

--- crates/front/src/views/pages.rs
+++ crates/front/src/views/pages.rs
@@ -27,14 +27,13 @@ pub async fn news_page (
     state: web::Data<AppState>, 
     req: HttpRequest
 ) -> actix_web::Result<HttpResponse> {
-    let (is_desctop, is_ajax, limit, offset) = get_device_and_ajax_and_limit_offset(state, &req, 20);
+    let (is_desctop, is_ajax, limit, offset) = get_device_and_ajax_and_limit_offset(state.clone(), &req, 20);
     let _request_user: User;
-    let c_state = state.clone();
     _request_user = User {
-        id:       *c_state.user_id.lock().unwrap(),
-        name:     (*c_state.user_name.lock().unwrap()).to_string(),
-        link:     (*c_state.user_link.lock().unwrap()).to_string(),
-        s_avatar: (*c_state.user_image.lock().unwrap()).to_string(),
+        id:       *state.user_id.lock().unwrap(),
+        name:     (*state.user_name.lock().unwrap()).to_string(),
+        link:     (*state.user_link.lock().unwrap()).to_string(),
+        s_avatar: (*state.user_image.lock().unwrap()).to_string(),
     };
     
     //let object_list: Vec<WallObject> = Vec::new();
