34fd46624aa07a89b869805cdc7f87691960cb9e
interesnij <ochkarik1983@mail.ru>
2022-12-23 17:44:22 +0300

    J

--- crates/front/src/views/pages.rs
+++ crates/front/src/views/pages.rs
@@ -31,8 +31,8 @@ pub async fn news_page (
     let _request_user: User;
     _request_user = User {
         id:       *state.user_id.lock().unwrap(),
-        name:     *state.user_name.lock().unwrap().clone(),
-        link:     *state.user_link.lock().unwrap().clone(),
+        name:     *state.user_name.lock().unwrap().to_string(),
+        link:     *state.user_link.lock().unwrap().to_string(),
         s_avatar: *state.user_image.lock().unwrap().as_ref(),
     };
     
