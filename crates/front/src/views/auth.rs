f49e820d5fb804ad7e04c9d6c7ca3dec1feaadbd
interesnij <ochkarik1983@mail.ru>
2022-12-24 15:58:13 +0300

    J

--- crates/front/src/views/auth.rs
+++ crates/front/src/views/auth.rs
@@ -57,7 +57,7 @@ pub async fn phone_send (
         &*data.borrow_mut(),
         ide 
     ).await?;
-    Ok(Json(_res))
+    Ok(res)
 }
 
 pub async fn mobile_signup(ide: Option<Identity>, req: HttpRequest) -> actix_web::Result<HttpResponse> {
