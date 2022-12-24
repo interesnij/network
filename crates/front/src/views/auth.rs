d1a1ba97c123a166cd1ef5c803e82bc5e153e372
interesnij <ochkarik1983@mail.ru>
2022-12-24 16:14:31 +0300

    J

--- crates/front/src/views/auth.rs
+++ crates/front/src/views/auth.rs
@@ -50,13 +50,13 @@ pub struct RespParams {
 pub async fn phone_send (
     ide: Identity,
     mut data: PhoneParams,
-) -> dyn Responder<Body = Type> { 
-    let res = request_post::<PhoneParams, RespParams> (
-        USERURL.to_owned() + &"/login".to_string(),
-        &*data.borrow_mut(),
-        ide 
-    ).await;
-    HttpResponse::Ok().body(res)
+) -> Responder { 
+    //let res = request_post::<PhoneParams, RespParams> (
+    //    USERURL.to_owned() + &"/login".to_string(),
+    //    &*data.borrow_mut(),
+    //    ide 
+    //).await;
+    HttpResponse::Ok().body("ok")
 }
 
 pub async fn mobile_signup(ide: Option<Identity>, req: HttpRequest) -> actix_web::Result<HttpResponse> {
