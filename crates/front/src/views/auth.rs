e70cdea61ebac0231da93e7a99cde5e705971e0b
interesnij <ochkarik1983@mail.ru>
2022-12-24 15:04:32 +0300

    J

--- crates/front/src/views/auth.rs
+++ crates/front/src/views/auth.rs
@@ -52,11 +52,12 @@ pub async fn phone_send (
     data: PhoneParams,
     state: web::Data<AppState>,
 ) -> Result<i16, Error> {
-    request_post::<PhoneParams, RespParams> (
+    let res = request_post::<PhoneParams, RespParams> (
         USERURL.to_owned() + &"/login".to_string(),
         &*data.borrow_mut(),
         ide 
-    ).await?
+    ).await?;
+    ok(res)
 }
 
 pub async fn mobile_signup(ide: Option<Identity>, req: HttpRequest) -> actix_web::Result<HttpResponse> {
