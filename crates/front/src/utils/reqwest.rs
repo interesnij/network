2c2ef664540f429f52d56433584fb881294d9d8d
interesnij <ochkarik1983@mail.ru>
2022-12-26 14:12:00 +0300

    J

--- crates/front/src/utils/reqwest.rs
+++ crates/front/src/utils/reqwest.rs
@@ -30,8 +30,8 @@ pub fn get_token(state: Data<AppState>)-> Option<String> {
 }
 
 pub fn is_authenticate(state: Data<AppState>)-> bool {
-    println!("token state {}", state.token.lock().unwrap().to_string() == "".to_string());
-    println!("token state empty {}", !state.token.lock().unwrap().is_empty());
+    println!("token state {}", state.token.lock().unwrap() == "");
+    println!("token state empty {}", state.token.lock().unwrap().is_empty());
     println!("token storage {}", web_local_storage_api::get_item("token").expect("E.").is_some());
     return !state.token.lock().unwrap().is_empty() || web_local_storage_api::get_item("token").is_ok();
 } 
