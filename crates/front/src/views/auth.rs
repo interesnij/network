884e9f543fa06820d684d877187c7b40bc3a9861
interesnij <ochkarik1983@mail.ru>
2022-12-27 07:37:00 +0300

    J

--- crates/front/src/views/auth.rs
+++ crates/front/src/views/auth.rs
@@ -53,17 +53,17 @@ pub struct RespParams {
 pub async fn phone_send (
     app_state: web::Data<AppState>,
     mut data: Json<PhoneParams>,
-) -> Json<Result<RespParams, u16>> { 
+) -> Result<Json<RespParams>, Json<u16>> { 
     let res = request_post::<PhoneParams, RespParams> (
         USERURL.to_owned() + &"/phone_send".to_string(),
         //&*data.borrow_mut(),
         &data,
         app_state,
     ).await;
-    match Json(res {
-        Ok(ok) => Ok(ok),
-        Err(err) => Err(err),
-    })
+    match res {
+        Ok(ok) => Ok(Json(ok)),
+        Err(err) => Err(Json(err)),
+    }
 }
 
 pub async fn mobile_signup(state: web::Data<AppState>, req: HttpRequest) -> actix_web::Result<HttpResponse> {
