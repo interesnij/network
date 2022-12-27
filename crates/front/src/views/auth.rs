ef38b50fd77ee44d3ae77a443859b95045952799
interesnij <ochkarik1983@mail.ru>
2022-12-27 08:07:45 +0300

    J

--- crates/front/src/views/auth.rs
+++ crates/front/src/views/auth.rs
@@ -57,7 +57,7 @@ pub struct ErrorParams {
 pub async fn phone_send (
     app_state: web::Data<AppState>,
     mut data: Json<PhoneParams>,
-) -> Json(RespParams) { 
+) -> Json<RespParams> { 
     let res = request_post::<PhoneParams, RespParams> (
         USERURL.to_owned() + &"/phone_send".to_string(),
         //&*data.borrow_mut(),
