9de102d327e268206ce17175f1e34ef953fb37b0
interesnij <ochkarik1983@mail.ru>
2022-12-28 10:14:15 +0300

    J

--- crates/users/src/utils/crypto.rs
+++ crates/users/src/utils/crypto.rs
@@ -1,4 +1,4 @@
-use actix_web::web::block;
+use actix_web::{web::block, HttpRequest};
 use chrono::{Duration, Utc};
 use jsonwebtoken::{decode, encode, DecodingKey, EncodingKey, Header, Validation};
 use serde::{Deserialize, Serialize};
@@ -61,6 +61,41 @@ pub async fn verify_jwt(_token: String, _secret: &String)-> Result<Claims, u16>{
     }
 
     Ok(claims)
+}
+
+fn get_auth_header<'a>(req: &'a HttpRequest) -> Option<&'a str> {
+    return req.headers().get("authorization")?.to_str().ok();
+}
+
+pub async fn is_auth(req: &HttpRequest, _secret: &String)-> Result<i32, u16>{
+    let jwt_key = _secret.clone();
+    let _token: String;
+    let _token_some = get_auth_header(&req);
+    if _token_some.is_some() {
+        _token = _token_some.unwrap();
+    }
+    else {
+        _token = String::new();
+    }
+    let claims = block(move || {
+        let decoding_key = DecodingKey::from_secret(jwt_key.as_bytes());
+
+        decode::<Claims>(&_token, &decoding_key, &Validation::default())
+    })
+    .await
+    .unwrap();
+    if let Err(_) = claims {
+        return Err(403);
+    }
+
+    //log::info!("Headers: {:?}", claims.as_ref().unwrap().header);
+    let claims = claims.unwrap().claims;
+
+    if claims.exp < Utc::now().timestamp(){
+        return Err(419);
+    }
+
+    Ok(claims.id)
 }
 
 pub async fn get_user_id(_token: String, _secret: &String) -> i32 {
