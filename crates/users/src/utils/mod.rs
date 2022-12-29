609cd2b13b5a2c774e730dcd022cc0dcd5f9a246
interesnij <ochkarik1983@mail.ru>
2022-12-29 12:18:18 +0300

    J

--- crates/users/src/utils/mod.rs
+++ crates/users/src/utils/mod.rs
@@ -170,7 +170,7 @@ pub fn get_user_owner_data (
                 match Authorization::<Bearer>::parse(req) {
                     Ok(ok) => {
                         let token = ok.as_ref().token().to_string();
-                        return match verify_jwt(token, "users") {
+                        return match verify_jwt(token, &"users").await {
                             Ok(ok) => ok.id,
                             Err(_) => Some("401 Unauthorized"),
                         }
