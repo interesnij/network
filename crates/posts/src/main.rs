1e0739bf42c4ef3bf443c17e766abd96d9fab145
interesnij <ochkarik1983@mail.ru>
2022-12-29 17:24:07 +0300

    J

--- crates/posts/src/main.rs
+++ crates/posts/src/main.rs
@@ -29,7 +29,7 @@ async fn main() -> std::io::Result<()> {
         key: Arc::new(env::var("KEY").unwrap()),
     };
 
-    HttpServer::new(|| {
+    HttpServer::new(move || {
         let cors = Cors::default()
             .allowed_origin("194.58.90.123:8000")
             .allowed_methods(vec!["GET", "POST"])
