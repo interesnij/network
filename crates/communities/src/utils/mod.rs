d81233dfda9b9aba6950558d4dc68d63f5c7a7b6
interesnij <ochkarik1983@mail.ru>
2023-01-02 19:52:52 +0300

    J

--- crates/communities/src/utils/mod.rs
+++ crates/communities/src/utils/mod.rs
@@ -150,7 +150,43 @@ pub fn get_moderation(id: i32) -> Result<Moderated, Error> {
 pub fn get_community_permission(community: &Community, user_id: i32)
     -> (bool, String) {
 
-    if community.types > 10 {
+    let request_user = get_user(user_id).expect("E.");
+    if request_user.types > 30 {
+        if request_user.is_closed() {
+            return (false, "Ваш профиль заблокирован за нарушение правил сайта".to_string())
+        }
+        else if request_user.is_deleted() {
+            return (false, "Ваш профиль удален".to_string())
+        }
+        else if request_user.is_suspended() {
+            return (false, "Ваш профиль будет разморожен ".to_string() + &request_user.get_longest_penalties())
+        }
+        else { return (false, "Закрыто".to_string())}
+    }
+    else if community.types > 20 {
+        if community.is_closed() {
+            return (false, community.name.clone() + &": сообщество заблокировано за нарушение правил сайта".to_string())
+        }
+        else if community.is_deleted() {
+            return (false, community.name.clone() + &": сообщество удалено".to_string())
+        }
+        else if community.is_suspended() {
+            return (false, community.name.clone() + &": сообщество будет разморожено ".to_string() + &community.get_longest_penalties())
+        }
+        else { return (false, "Закрыто".to_string())}
+    }
+    else if community.is_user_in_ban(user_id) {
+        return (false, community.name.clone() + &": сообщество добавило Вас в чёрный список".to_string())
+    }
+    else { 
+        return (true, "Открыто".to_string())
+    }
+}
+
+pub fn get_anon_community_permission(community: &Community)
+    -> (bool, String) {
+
+    if community.types > 20 {
         if community.is_closed() {
             return (false, community.name.clone() + &": сообщество заблокировано за нарушение правил сайта".to_string())
         }
