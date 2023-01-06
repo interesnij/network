791cad22ba2abd54feea6511c1a2c0a570c0cb25
interesnij <ochkarik1983@mail.ru>
2023-01-06 06:59:53 +0300

    J

--- crates/photos/src/views/pages.rs
+++ crates/photos/src/views/pages.rs
@@ -2196,7 +2196,7 @@ pub async fn community_notifies_page (
     let params_some = web::Query::<SmallCommunityData>::from_query(&req.query_string());
     if params_some.is_ok() {
         let params = params_some.unwrap();
-        let (err, user_id, community_id) = get_user_owner_data(&req, state, params.token.clone(), 0).await;
+        let (err, user_id, community_id) = get_owner_data(&req, state, params.token.clone(), 0).await;
         if err.is_some() {
             let body = serde_json::to_string(&ErrorParams {
                 error: err.unwrap(),
